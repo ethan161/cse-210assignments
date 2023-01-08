d351624b763ca30816da73f7fc7437ca8c9ed6e9
ethan161 <bla19046@byui.edu>
2023-01-07 21:35:44 -0700

    Week1 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("Please insert the percentage you earned: ");
+        string percentage = Console.ReadLine();
+        int number = int.Parse(percentage);
+        string letter = "";
+
+        if (number >= 90)
+            {
+                letter = "A";
+            }
+        else if (number < 90 && number >=80)
+            {
+                letter = "B";
+            }
+        else if (number < 80 && number >= 70)
+            {
+                letter = "C";
+            }
+        else if (number < 70 && number >= 60)
+            {
+                letter = "D";
+            }
+        else if (number < 60)
+            {
+                letter = "F";
+            }
+
+        Console.WriteLine($"You earned a(n) {letter}.");
+        if (number >= 70)
+        {
+            Console.WriteLine("You passed.");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass.");
+        }
+}
 }
