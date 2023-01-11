9f10bb6460412a875680ff0e4ffd3572a022973e
ethan161 <bla19046@byui.edu>
2023-01-11 14:09:24 -0700

    week 3 prep

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
+
 {
     static void Main(string[] args)
+    
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            input = int.Parse(Console.ReadLine());
+            numbers.Add(input);
+        } while (input != 0);
+        int length = numbers.Count;
+        int sum;
+        sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {sum/length}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
     }
 }
