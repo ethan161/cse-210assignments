571970b673561ad71099ddcf3c89120e558158a7
ethan161 <bla19046@byui.edu>
2023-01-11 14:08:40 -0700

    week 3 prep

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
+        int input = 0;
+
+        Console.WriteLine($"What is the magic number? {number}");
+        do
+        {
+            Console.Write("What is your guess? ");
+        input = int.Parse(Console.ReadLine());
+        if (input > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (input < number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (input == number)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        } while (number != input);
+        
+    }   
 }
