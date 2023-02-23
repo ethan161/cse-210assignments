997e7a6d7bd631a33fd8d0c53789bb42bd027074
ethan161 <bla19046@byui.edu>
2023-02-23 10:48:32 -0700

    Framework

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Activity
+{
+    Random rnd = new Random();
+    string name = "";
+    string description = "";
+    string duration = "";
+    public void DisplayStartMessage()
+    {
+
+    }
+    public void DisplayEndMessage()
+    {
+
+    }
+    public void Pause()
+    {
+
+    }
+    public void Timer()
+    {
+        
+    }
+}
---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class BreathingActivity : Activity
+{
+    Activity activity = new Activity();
+
+    public void RunActivity()
+    {
+
+    }
+    public void BreathActivity()
+    {
+        
+    }
+}
