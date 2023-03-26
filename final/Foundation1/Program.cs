27a4db1f2611667088453fb609c44e018cd436a3
ethan161 <bla19046@byui.edu>
2023-03-25 19:05:20 -0600

    Funcionality completed

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -10,29 +10,43 @@ class Program
 
     public void Display()
     {
-        Video newVideo = CreateVideo();
-        Console.WriteLine($"'{newVideo[0]}' by {newVideo[1]} ({newVideo[2]/60}:{newVideo[2]%60})");
-        Console.WriteLine($"Comments: ({.NumberOfComments()})");
-        foreach (string comment in comments)
+
+        List<Video> videos = CreateVideos();
+        foreach (Video newVideo in videos)
         {
-            Console.WriteLine($"{comment}")
+            Console.WriteLine();
+            Console.WriteLine($"'{newVideo.Title}' by {newVideo.Author} ({newVideo.Length/60}:{newVideo.Length%60})");
+            Console.WriteLine($"{newVideo.NumberOfComments()} comments: ");
+            foreach (Comment comment in newVideo.Comments)
+            {
+                Console.WriteLine($"{comment.Author}: {comment.Text}");
+            }
         }
     }
 
-    public List<Comment> CreateComments()
+    public List<Video> CreateVideos()
     {
-        List<Comment> comments = new List<Comment>();
-        comments.Add(new Comment("John", "Nice video!"));
-        comments.Add(new Comment("Susie", "When is your next video?"));
-        comments.Add(new Comment("Bill", "Very cool demonstration."));
-        return comments;
-    }
+        Video video1 = new Video("How to Make a Pancake", "Joe34", 145);
+        video1.Comments.Add(new Comment("John", "Nice video!"));
+        video1.Comments.Add(new Comment("Susie", "When is your next video?"));
+        video1.Comments.Add(new Comment("Bill", "Very cool demonstration."));
 
-    public Video CreateVideo()
-    {
-        Video newVideo = new Video("How to Make a Pancake", "Joe34", 145);
-        CreateComment();
-        return newVideo;
+        Video video2 = new Video("Swapping a Tire", "Mechanic21", 1513);
+        video2.Comments.Add(new Comment("Kim", "Thanks for the video!"));
+        video2.Comments.Add(new Comment("Susan", "Does this work on a truck?"));
+        video2.Comments.Add(new Comment("William", "Very good job."));
+
+        Video video3 = new Video("Playing my New Game", "Gamer201", 1002);
+        video3.Comments.Add(new Comment("Paul", "Thanks for the gameplay!"));
+        video3.Comments.Add(new Comment("Julie", "Does this work on an Xbox?"));
+        video3.Comments.Add(new Comment("Davis", "You should play this again!"));
+
+        List<Video> videos = new List<Video>();
+        videos.Add(video1);
+        videos.Add(video2);
+        videos.Add(video3);
+
+        return videos;
     }
 
 }
